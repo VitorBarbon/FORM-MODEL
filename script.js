f910313ae49a46075d274b3ad188727b267cca22
VitorBarbon <dev_vitorbarbon@gmail.com>
2022-05-12 14:35:36 -0300

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('#form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('passwordConfirm');
+
+form.addEventListener( 'submit',(e) => {
+  e.preventDefault();
+  
+})
